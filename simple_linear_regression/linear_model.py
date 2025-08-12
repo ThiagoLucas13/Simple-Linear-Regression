419ffec46ce036b9b7cacdb3ebd27b9357614e37
Thiago Lucas <thiagolucassilva@outlook.com.br>
2025-08-11 22:31:25 -0300

    Some methods and processes were modularized but it's not ended yet

---
+++ simple_linear_regression/linear_model.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+
+class SimpleLinearRegression:
+    def __init__(self, lr: float, tolerance: float, n_iterations: float, metric: str, method:str) -> None:
+        '''
+        '''
+        # Initialization
+        self.tolerance = tolerance
+        self.max_iterations = n_iterations
+        self.total_iterations = 0
+        self.lr = lr
+        self.metric = metric
+        self.method = method
+        # Parameters of Regression
+        self.w = None
+        self.b = None
+        self.weights = list()
+        self.costs = list()
+
+    def __f(self, X: np.array, w: float, b: float) -> float:
+        return w * X + b
+
+    def __loss_function(self, X: np.array, y: np.array, w: float, b: float, metric: str) -> float:
+        # Mean Squared Error (MSE)
+        if metric.strip().lower() == "mse":
+            return (1/(2 * X.shape[0])) * np.sum( (self.__f(X, w, b) - y)**2 )
+
+    def __update_coefficients(self, X: np.array, y: np.array, w: float, b: float, lr: float, metric: str, method: str) -> float:
+    # Gradient Descent (GD)
+        if method.strip().lower() == "gd":
+            def d_loss_function(X: np.array, y: np.array, w: float, b: float, metric: str) -> float:
+                # Derivatives of Mean Squared Error
+                if metric.strip().lower() == "mse":
+                    dw = (1/X.shape[0]) * np.sum( (self.__f(X, w, b) - y) * X )
+                    db = (1/X.shape[0]) * np.sum( self.__f(X, w, b) - y )
+                    return dw, db
+        
+            dw, db = d_loss_function(X, y, w, b, metric)
+            aux_w = w - lr * dw
+            aux_b = b - lr * db
+            return aux_w, aux_b
+
+    def __gradient_descent(self, X: np.array, y: np.array, lr: float, metric: str):
+        # Generating initial values for w and b
+        w, b = 6, 7
+        # Initial cost
+        cost = self.__loss_function(X, y, w, b, metric)
+        self.costs.append(cost)
+        while (cost > self.tolerance) and (i < self.max_iterations):
+            w, b = self.__update_coefficients(X, y, w, b, lr, metric, method="gd")    # Update the coefficients of f
+            cost = self.__loss_function(X, y, w, b, metric)                           # The total cost related to new coefficients
+            # Saving the history
+            self.weights.append((w, b))
+            self.costs.append(cost)
+            self.total_iterations += 1
+
+    def fit(self, X: np.array, y: np.array) -> None:
+        if self.method.strip().lower() == "gd":
+            return self.__gradient_descent(X, y, self.lr, self.metric)
+
+    def predict(self, X: np.array) -> np.array:
+        return self.__f(X=X, w=self.w, b=self.b)
+
+    # def evaluate(self, y: np.array) -> float:
+    #     return 
+
+    def info(self) -> dict:
+        infos = {"tolerance": self.tolerance, "user_limit_iterations": self.max_iterations, "model_real_iterations_executed": self.total_iterations,
+                "learning_rate": self.lr, "metric": self.metric, "method": self.method, "weights": (self.w, self.b),
+                "weights_history": self.weights, "costs_history": self.costs}
+        return infos
---
+++ simple_linear_regression/utils.py
@@ -0,0 +1,32 @@
+from os import path, makedirs
+import numpy as np
+
+PREFIX_DATA_PATH = "../data/"
+
+def normalization(data: np.array) -> np.array:
+    min_value, max_value = np.min(data), np.max(data)
+    diff = max_value - min_value
+    return (data - min_value) / diff if diff != 0 else data
+
+def standardization(data: np.array) -> np.array:
+    mean_data = np.mean(data)
+    std_data = np.std(data)
+    return (data - mean_data) / std_data if std_data != 0 else data
+
+def save_data(use: str, X, y) -> None:
+    use = use.strip().lower()
+    try:
+        if use not in ["train", 'validation', "test"]:
+           raise TypeError("Only 'train', 'validation' and 'test' uses are avaiable for the parameter 'use'.")
+    
+        folder_data = PREFIX_DATA_PATH + f"{use}/"
+        # Create directory if it doesn't exist
+        makedirs(path.dirname(folder_data), exist_ok=True)
+
+        data = np.column_stack((X, y))
+        save_path = folder_data + f"{use}_data"
+        np.save(save_path + ".npy", data)
+        np.savetxt(save_path + ".csv", data, delimiter=",", fmt="%.3f")
+        
+    except(TypeError, ValueError) as e:
+        print(e)
